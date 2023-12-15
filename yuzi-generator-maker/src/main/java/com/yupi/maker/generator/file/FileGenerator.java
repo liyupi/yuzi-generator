f0930aa09a8b4fb5cdb4aec15dd436b4383087a4
liyupi <592789970@qq.com>
2023-12-15 13:43:32 +0800

    第 7 期教程：模板制作工具开发

--- yuzi-generator-maker/src/main/java/com/yupi/maker/generator/file/FileGenerator.java
+++ yuzi-generator-maker/src/main/java/com/yupi/maker/generator/file/FileGenerator.java
@@ -1,6 +1,5 @@
 package com.yupi.maker.generator.file;
 
-import com.yupi.maker.model.DataModel;
 import freemarker.template.TemplateException;
 
 import java.io.File;
@@ -31,13 +30,5 @@ public class FileGenerator {
         String inputDynamicFilePath = projectPath + File.separator + "src/main/resources/templates/DataModel.java.ftl.ftl";
         String outputDynamicFilePath = outputPath + File.separator + "acm-template/src/com/yupi/acm/MainTemplate.java";
         DynamicFileGenerator.doGenerate(inputDynamicFilePath, outputDynamicFilePath, model);
-    }
-
-    public static void main(String[] args) throws TemplateException, IOException {
-        DataModel dataModel = new DataModel();
-        dataModel.setAuthor("yupi");
-        dataModel.setLoop(false);
-        dataModel.setOutputText("求和结果：");
-        doGenerate(dataModel);
     }
 }
